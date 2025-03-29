67aa54f1daa74b655bf1e7d1066763b702be51df
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2025-03-28 21:27:08 -0500

    Add initial project structure and resources for augmented mobile application

---
+++ app/src/main/java/com/example/augmented_mobile_application/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.augmented_mobile_application
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.augmented_mobile_application.ui.theme.Augmented_mobile_applicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Augmented_mobile_applicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Augmented_mobile_applicationTheme {
+        Greeting("Android")
+    }
+}
